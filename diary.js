bb712e45d94ba2e84c1ae6a04df8a9c8985d79d9
slhukss <74009459+slhukss@users.noreply.github.com>
2024-07-23 18:22:44 +0100

    Add files via upload

--- diary.js
+++ diary.js
@@ -1,17 +1,25 @@
 // メニューリスト作成
-function makeDiaryList(displayArry) {
+function makeDiaryList(displayArry, number) {
+    var newNumber = number;
+    var basedNumber = 0;
     // リストの親要素取得
-    const diary = $(".diary");
+    const diaryEntry = $(".entry_list");
+    const diaryMain = $('.diary');
+    const judge = $('.entry_list li').get(0);
+    if (judge) {
+        basedNumber = newNumber;
+        newNumber = newNumber + 3;
+    }
     // 表示する数だけ繰り返す
-    for (let i = 0; i < displayArry.length; i++) {
+    for (let i = basedNumber; (i < newNumber) && (i < displayArry.length); i++) {
         // 日記オブジェクト
         const diaryObj = displayArry[i];
+        console.log(diaryObj);
         // 日記の日付
         const diary_date = diaryObj.date;
         // 日記の写真が何枚あるか確認
         let diary_photos = null;
         for (let l = 0; l < diaryObj.photos.length; l++) {
-            console.log(diaryObj.photos[l]);
             if (diary_photos == null) {
                 diary_photos = "<img src='image/diary/" + diaryObj.id + "/" + diaryObj.photos[l] + ".jpg' alt='" + diaryObj.photos[l] + "'>"
             } else {
@@ -21,18 +29,33 @@ function makeDiaryList(displayArry) {
         // 日記本文
         const diary_text = diaryObj.text;
         // リスト要素作成
-        const diary_list = "<article><h2 class='article-title'>" + diary_date + "</h2>"
+        const diary_list = "<li><article class='entry'><h2 class='article-title'>" + diary_date + "</h2>"
                             + "<div class='img_display'>" + diary_photos + "</div>"
                             + "<p class='text'>" + diary_text + "</p>"
-                            + "</article>"
+                            + "</article></li>";
         // リストに追加
-        diary.append(diary_list);
+        diaryEntry.append(diary_list);
+       if (displayArry.length - i == 1) {
+        $('.more').hide();
+       }
+    }
+    if(!$('.more').length){
+        console.log("more button needs to be displayed!")
+        diaryMain.append("<div class='more'><button>next entries</button></div>");
     }
 }
 
 // メニュー画面の初めの画面表示用
 $(document).ready(() => {
     $.getJSON("diary.json", (data) => {
-        makeDiaryList(data);
+        makeDiaryList(data, 3);
+    });
+});
+
+// もっと読むボタン
+$(document).on('click', '.more', function () {
+    var number = $('.entry').length;
+    $.getJSON("diary.json", (data) => {
+        makeDiaryList(data, number);
     });
 });
